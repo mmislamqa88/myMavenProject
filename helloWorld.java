71e86b4ef81d9e776f42a20fb3cb305f55334694
MD <mmislamqa@gmail.com>
2022-10-05 22:52:46 -0400

    maven project

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+class helloWorld {
+   
+public static void main(String [] args){
+   System.out.println("Hello World");
+   System.out.println("Hi, I am Moynul");
+   System.out.println("This is Devops class");
+}
+
+}
